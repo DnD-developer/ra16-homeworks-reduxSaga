0ee0a117606cf1144872b3a5e99d4505fee4f492
DND-dev <dev.work@dnd-projects.ru>
2023-09-12 20:47:08 +0300

    create design

--- src/widgets/SearchWidget/SearchWidget.jsx
+++ src/widgets/SearchWidget/SearchWidget.jsx
@@ -3,14 +3,34 @@ import * as styles from "./SearchWidget.module.css"
 //hooks
 import useInput from "../../hooks/useInput"
 //components
-import Search from "../../components/Search"
+import List from "../../components/List/List"
+import { SearchItem } from "../../components/Search-item/SearchItem"
+import Search from "../../components/Search/Search"
 
 export const SearchWidget = () => {
 	const [inputValue, onInput] = useInput()
 
+	const data = [
+		{
+			title: "Ant Design Title 1"
+		},
+		{
+			title: "Ant Design Title 2"
+		},
+		{
+			title: "Ant Design Title 3"
+		},
+		{
+			title: "Ant Design Title 4"
+		}
+	]
+
 	return (
 		<div className={styles.widget}>
 			<Search value={inputValue} onInput={onInput} />
+			<div className={styles.content}>
+				<List data={data}>{({ title }) => <SearchItem dataText={title} />}</List>
+			</div>
 		</div>
 	)
 }
